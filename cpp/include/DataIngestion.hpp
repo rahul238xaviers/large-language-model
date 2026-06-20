b723af095e3043bcc8f0765ca0dd1e3316f6aa0f
rahul238xaviers <rahul238xaviers@gmail.com>
2026-06-21 00:27:50 +1000

    refactor: migrate documentation to Python and C++ directories and update DataIngestion interfaces

--- cpp/include/DataIngestion.hpp
+++ cpp/include/DataIngestion.hpp
@@ -22,8 +22,9 @@ private:
   int current_shard_idx_ = 0;
   int current_batch_idx_ = 0;
   std::unordered_map<std::string, int> vocab_;
-  bool load_parquet_shard(const std::string &file_path);
-  void tokenize_cpp_corpus(std::shared_ptr<arrow::Table> table);
+  arrow::Status load_parquet_shard(const std::string &file_path);
+  void tokenize_cpp_corpus(std::shared_ptr<arrow::Table> table,
+                           const std::string col);
   void generate_training_sequences();
   static void bpe_encode(const std::string &text, std::vector<int> &tokens,
                          const std::unordered_map<std::string, int> &vocab);
--- cpp/src/DataIngestion.cpp
+++ cpp/src/DataIngestion.cpp
@@ -1,17 +1,73 @@
 #include "DataIngestion.hpp"
+#include "algorithm"
+#include <arrow/api.h>
+#include <arrow/io/file.h>
+#include <arrow/memory_pool.h>
+#include <filesystem>
 #include <iostream>
+#include <memory>
+#include <parquet/arrow/reader.h>
 
 DataIngestion::DataIngestion(const std::string &data_dir,
                              size_t max_shard_bytes) {
   data_dir_ = data_dir;
   max_shard_bytes_ = max_shard_bytes;
+
+  for (const auto &entry : std::filesystem::directory_iterator(data_dir)) {
+
+    if (entry.is_regular_file() && entry.path().extension() == ".parquet") {
+
+      file_paths_.push_back(entry.path().string());
+    }
+  }
+  std::sort(file_paths_.begin(), file_paths_.end());
 }
-bool DataIngestion::load_parquet_shard(const std::string &file_path) {
+
+// Uses the macro functions available in the arrow C++ library
+arrow::Status DataIngestion::load_parquet_shard(const std::string &file_path) {
+
   std::cout << "Loading shard: " << file_path << std::endl;
-  return true;
+
+  // This would check if the file can be opened for reading or not
+  ARROW_ASSIGN_OR_RAISE(std::shared_ptr<arrow::io::ReadableFile> infile,
+                        arrow::io::ReadableFile::Open(file_path));
+
+  // This would open the file and load the data in default memory pool
+  ARROW_ASSIGN_OR_RAISE(
+      std::unique_ptr<parquet::arrow::FileReader> arrow_reader,
+      parquet::arrow::OpenFile(infile, arrow::default_memory_pool()))
+
+  // Read the file from the parquet file into a table
+  std::shared_ptr<arrow::Table> table;
+  ARROW_ASSIGN_OR_RAISE(table, arrow_reader->ReadTable());
+
+  // Push the table into the shards_
+  shards_.push_back(table);
+  return arrow::Status::OK();
 }
-void DataIngestion::tokenize_cpp_corpus(std::shared_ptr<arrow::Table> table) {
+
+void DataIngestion::tokenize_cpp_corpus(std::shared_ptr<arrow::Table> table,
+                                        const std::string col) {
   std::cout << "Tokenizing C++ corpus..." << std::endl;
+  std::shared_ptr<arrow::ChunkedArray> chunked_array =
+      table->GetColumnByName(col);
+
+  if (!chunked_array) {
+    std::cerr << "Column '" << col << "' not found!" << std::endl;
+    return;
+  }
+
+  for (int i = 0; i < chunked_array->num_chunks(); i++) {
+    std::shared_ptr<arrow::Array> chunk = chunked_array->chunk(i);
+
+    auto string_array = std::static_pointer_cast<arrow::StringArray>(chunk);
+
+    for (int64_t j = 0; j < string_array->length(); ++j) {
+      if (string_array->IsValid(j)) {
+        std::string text = string_array->GetString(j);
+      }
+    }
+  }
 }
 void DataIngestion::generate_training_sequences() {
   std::cout << "Generating training sequences..." << std::endl;
