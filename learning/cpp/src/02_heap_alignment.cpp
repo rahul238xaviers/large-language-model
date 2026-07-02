3293255744e511365075d10b4903edefea8ae242
rahul238xaviers <rahul238xaviers@gmail.com>
2026-07-03 00:32:21 +1000

    docs: add explanatory comments for posix_memalign usage in heap alignment example

--- learning/cpp/src/02_heap_alignment.cpp
+++ learning/cpp/src/02_heap_alignment.cpp
@@ -4,7 +4,15 @@
 int main() {
 
   void *my_ptr = nullptr;
-
+  // posix_memalign allocates memory in heap that is aligned to the specified
+  // boundary
+  // Parameters: 1. address of pointer to be allocated
+  //             2. alignment boundary
+  //             3. size of memory to be allocated
+  // For page aligned which is 16KB it helps in doing a buffer without copy to
+  // GPU.
+  // 16384 = 16 KB
+  // 32768 = 32 KB
   int success_fail = posix_memalign(&my_ptr, 16384, 32768);
   if (success_fail == 0) {
     std::cerr << "Memory allocation Passed" << std::endl;
